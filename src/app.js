f2a03104370775d4bcfdba23eb7dc303ac6f33fe
PGabriel20 <bielporreca@gmail.com>
2022-02-17 00:12:30 -0300

    Fixing semantics

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
-const {prefix, token, bot_info} = require('./config/config')
+const commando = require('discord.js-commando')
+const bot = new commando.Client()
+const {token} = require('./config/config')
 
-bot.registry.registerGroup('bicho','Beast');
+bot.registry.registerGroup('bicho','Beast')
 bot.registry.registerCommandsIn(__dirname + '/commands')
-
 
 
 bot.on('ready', ()=>{
